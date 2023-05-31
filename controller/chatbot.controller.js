ac15da0d53b7d711d449b31cae16ebb225fd5ffd
Niloy173 <niloysaha173@gmail.com>
2023-05-31 11:39:52 +0600

    postwebHook modified

--- controller/chatbot.controller.js
+++ controller/chatbot.controller.js
@@ -23,26 +23,47 @@ let getWebhook = (req,res,next) => {
 
 let postWebhook = (req,res,next) => {
 
+  // Parse the request body from the POST
   let body = req.body;
 
-  if(body.object === 'page') {
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
 
-    body.entry.forEach(function(entry){
-      
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+          // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
 
+      // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
-      // console.log('Sender PSID: ' + sender_psid);
-
-     
-
+      console.log('Sender PSID: ' + sender_psid);
+      
     });
 
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
-  }else{
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+}
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
 }
 
 
